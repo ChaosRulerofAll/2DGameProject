2c7ba0c401a3b6fb28ca6d78ed95acfbb161db45
ChaosRulerofAll <ilya@soukoreff.com>
2023-11-23 16:45:05 -0500

    Fixed a bug with the movement
    if the platform started by moving downward it used to constantly switch direction and go nowhere.
    NOT ANYMORE!

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -6,26 +6,37 @@ using UnityEngine;
 public class Platform : ObjectMovement
 {
 
+    //Our moving thing
     [SerializeField] private GameObject platform;
 
+    //Where are we going?
     private Vector2 move;
 
+    //This is needed for boundary checks
+    private bool isEndAboveStart;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //What direction an I moving?
         move = new Vector2(GetEndLocation().position.x - GetStartLocation().position.x, GetEndLocation().position.y - GetStartLocation().position.y);
 
-        platform.GetComponent<Transform>().position = GetStartLocation().position;
+        //Set the object at the start point
+        //This was removed because what if you want an object out of faze of other objects
+        //platform.GetComponent<Transform>().position = GetStartLocation().position;
 
+        //How fast does it move?
         moveSpeed = 0.5f;
+
+        //Check if the object is moving up to start or down to start
+        //This is needed for the back-and-forth motion
+        isEndAboveStart = (GetEndLocation().position.y > GetStartLocation().position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Move in the right direction
         if (isGoingRight)
         {
             //Debug.Log("isGoingRight");
@@ -37,11 +48,28 @@ public class Platform : ObjectMovement
             platform.transform.Translate(-move * moveSpeed * Time.deltaTime);
         }
 
-        if (GetStartLocation().position.y > platform.transform.position.y 
-            || platform.transform.position.y > GetEndLocation().position.y)
-        {
-            isGoingRight = !isGoingRight;
-        }
 
+        //Checks for if we need to turn around
+        //if the object starts by moving up
+        if (GetStartLocation().position.y > platform.transform.position.y && isEndAboveStart)
+        {
+            isGoingRight = true;
+        }
+        
+        if (GetEndLocation().position.y < platform.transform.position.y && isEndAboveStart)
+        {
+            isGoingRight = false;
+        }
+        
+        //if the object starts by moving down
+        if (GetStartLocation().position.y < platform.transform.position.y && !isEndAboveStart)
+        {
+            isGoingRight = true;
+        }
+        
+        if (GetEndLocation().position.y > platform.transform.position.y && !isEndAboveStart)
+        {
+            isGoingRight = false;
+        }
     }
 }
