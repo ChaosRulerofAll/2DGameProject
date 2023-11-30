c1649170df920321deae3a9ea3271976b4b4ada5
ChaosRulerofAll <ilya@soukoreff.com>
2023-11-30 15:33:03 -0500

    Got some stuff that was lost because of github

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
@@ -12,9 +13,13 @@ public class Player : MonoBehaviour
     //Creates serialized fields for adjustable numbers
     [SerializeField] private float accelerationPower;
     [SerializeField] private float jumpPower;
+    [SerializeField] private float springPower;
     //Creates seriaized feilds that will help with detectinf if the player is on the ground
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask springLayer;
+
+    [SerializeField] private Vector2 SpawnPoint;
 
     //A boolean to check facing direction of player. Will be used for sprite
     private bool isFacingRight;
@@ -25,32 +30,37 @@ public class Player : MonoBehaviour
 
     [SerializeField] private Text UI;
 
+    public GameObject Watermelon;
+
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         melonCount = 0;
         maxMelons = 1;
+        transform.position = SpawnPoint;
+        //Instantiate(Watermelon, new Vector2(3.5f,0.5f), Quaternion.identity);
     }
 
     // Update is called at a fixed rate
     void FixedUpdate()
     {
         //Move left
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
         {
             body.AddForce(accelerationPower * -transform.right, ForceMode2D.Force);
         }
         //Move Right
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
         {
             body.AddForce(accelerationPower * transform.right, ForceMode2D.Force);
         }
         //Jump
-        if (Input.GetKey(KeyCode.W)&&isGrounded())
+        if ((Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.Space))&& isGrounded())
         {
             body.AddForce(jumpPower * transform.up, ForceMode2D.Force);
         }
+        //Debugging Code
         UI.text = "Melons: " + melonCount.ToString() + "/" + maxMelons.ToString();
     }
 
@@ -59,6 +69,12 @@ public class Player : MonoBehaviour
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
+    //Function for when player dies
+    private void die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -66,7 +82,19 @@ public class Player : MonoBehaviour
         if (collision.gameObject.tag == "Melon")
         {
             melonCount++;
-            Debug.Log("Current melons" + melonCount);
+            Debug.Log("Current melons " + melonCount);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Spring Collision Code
+        if (Physics2D.OverlapCircle(groundCheck.position, 0.2f, springLayer))
+        {
+            body.AddForce(springPower * transform.up, ForceMode2D.Force);
+        }
+        if (collision.gameObject.tag == "Spikes")
+        {
+            die();
         }
     }
 }
